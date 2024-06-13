6c1fede6d681031477c8a98e738b794d4b36d4ce
Felipe Prado <felipe.chprado@gmail.com>
2024-06-12 22:27:38 -0300

    dev 23

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date()
     const hora = data.getHours()
-    return hora >= 18 && hora < 22
+    return hora >= 18 && hora < 23
 }
 
 const footer = document.getElementById("footer")
